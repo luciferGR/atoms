18d36ae07eb2c6ba83370ba891ea3dedc8d5e093
gongrui <gongrui@bytedance.com>
2025-03-17 11:43:58 +0800

    add spec update

---
+++ spec_update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# 更新机器内网源repo
+set +e
+echo "CLOUD_BUILD_INFO_LOG: ************************"
+echo "CLOUD_BUILD_INFO_LOG: 执行pod内网源更新模板"
+
+echo "CLOUD_BUILD_INFO_LOG: 更新完成"
+echo "CLOUD_BUILD_INFO_LOG: ************************"
